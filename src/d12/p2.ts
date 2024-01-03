b2ebe557c5bc97b2e0a5d607c25b54e95251361c
Romain Theodet <romain.theodet@amersports.com>
2024-01-03 16:29:24 +0100

    feat: day 12 part 2 (WIP)

---
+++ src/d12/p2.ts
@@ -0,0 +1,51 @@
+import console from "console";
+import fs from "fs";
+
+console.log(
+	fs
+		.readFileSync(__dirname + "/input")
+		.toString()
+		.split("\n")
+		.map((s) => s.trim())
+		.filter((s) => s !== "")
+		.map((s) => s.split(" "))
+		.map(([left, right]) => [
+			new Array(5).fill(left).join("?"),
+			new Array(5).fill(right).join(","),
+		])
+		.map(
+			([left, right]) =>
+				[left, right.split(",").map((s) => parseInt(s, 10))] as const,
+		)
+		.map(
+			([left, right]) =>
+				[
+					left,
+					"^[.?]*" +
+						right.map((n) => `[?#]{${n}}`).join("[.?]+") +
+						"[.?]*$",
+				] as const,
+		)
+		.map(
+			([str, pattern]) =>
+				[str, (s: string) => new RegExp(pattern).test(s)] as const,
+		)
+		.map(
+			([str, isValid]) =>
+				[
+					str,
+					(
+						s: string,
+						check: (s: string, recurse: typeof check) => number,
+					) =>
+						!isValid(s)
+							? 0
+							: !s.includes("?")
+								? 1
+								: check(s.replace("?", "."), check) +
+									check(s.replace("?", "#"), check),
+				] as const,
+		)
+		.map(([str, check]) => check(str, check))
+		.reduce((a, b) => a + b, 0),
+);
